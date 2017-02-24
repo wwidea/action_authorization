1b5aa30ce81cd5511ebb18dd959f547e71666813
Aaron Baldwin <baldwina@brightwayslearning.org>
2017-02-24 11:03:21 -0700

    setup guardfile and test files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,11 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'action_authorization'
 
 require 'minitest/autorun'
+
+class ApplicationController
+  def self.helper_method(*_args)
+    # not implemented
+  end
+  
+  include ActionAuthorization
+end
