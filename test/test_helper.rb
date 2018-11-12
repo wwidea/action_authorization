2aedec57099921e5dd557b65652a492aa30423ec
Aaron Baldwin <baldwina@brightwayslearning.org>
2018-11-12 16:24:34 -0700

    add simplecov for test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test/'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'action_authorization'
 
@@ -7,22 +12,22 @@ class ApplicationController
   def self.helper_method(method)
     method
   end
-  
+
   include ActionAuthorization
-  
+
   def current_user
     User.new
   end
-  
+
   def action_name
     'show'
   end
 end
 
 class DocumentPolicy < ActionAuthorization::BasePolicy
-  
+
   private
-  
+
   def authorized?
     document.owner == user.name
   end
@@ -30,7 +35,7 @@ end
 
 class Document
   attr_accessor :owner
-  
+
   def initialize(owner: 'Audrey')
     self.owner = owner
   end
