67b449213b42734500705ee67472dd8410ce765b
Aaron Baldwin <baldwina@brightwayslearning.org>
2019-01-10 11:14:06 -0700

    remove extra spaces

--- test/action_authorization/base_policy_test.rb
+++ test/action_authorization/base_policy_test.rb
@@ -3,29 +3,29 @@ require 'test_helper'
 class ActionAuthorization::BasePolicyTest < Minitest::Test
   def test_should_initialize_with_user_and_object
     policy = document_policy
-    
+
     assert_equal 'Zachary', policy.user.name
     assert_equal 'Audrey',  policy.object.owner
   end
-  
+
   def test_should_create_alias_to_object_based_on_policy_class_name
     assert document_policy.document
   end
-  
+
   def test_action_authorization_should_default_to_false
     assert_authorized_action_methods(ActionAuthorization::BasePolicy.new('user', 'object'), false)
   end
-  
+
   def test_action_authorization_should_return_true
     assert_authorized_action_methods(DocumentPolicy.new(User.new, Document.new(owner: 'Zachary')), true)
   end
-  
+
   private
-  
+
   def document_policy
     DocumentPolicy.new(User.new, Document.new)
   end
-  
+
   def assert_authorized_action_methods(policy, value)
     %w(index? show? new? create? edit? update? destroy?).each do |method|
       assert_equal value, policy.send(method)
--- test/action_authorization_test.rb
+++ test/action_authorization_test.rb
@@ -6,29 +6,29 @@ class ActionAuthorizationTest < Minitest::Test
       authorize(Document.new)
     end
   end
-  
+
   def test_should_authorize_action
     assert authorize(Document.new(owner: 'Zachary'))
   end
-  
+
   def test_should_return_corresponding_policy_object
     assert_equal DocumentPolicy, policy(Document.new).class
   end
-  
+
   def test_should_return_instance_of_supplied_policy_class
     assert_equal DocumentPolicy, policy('test', DocumentPolicy).class
   end
-  
+
   def test_that_it_has_a_version_number
     refute_nil ::ActionAuthorization::VERSION
   end
-  
+
   private
-  
+
   def authorize(*args)
     ApplicationController.new.send(:authorize, *args)
   end
-  
+
   def policy(*args)
     ApplicationController.new.send(:policy, *args)
   end
